fe614391853ab6dd3181730d24d0b4ab0f2f2857
Miley Wright <dishaliekwright@yahoo.com>
2020-11-19 01:57:14 -0500

    JS test passing

--- javascript-questions/index.js
+++ javascript-questions/index.js
@@ -10,7 +10,8 @@ const superHeroes = require('./super-heroes.json');
  * ]
  */
 const getDCHeroes = (heroes) => {
-  return heroes;
+  return heroes.filter(hero => hero.publisher === 'Marvel Comics')
+  
 }
 
 /**
@@ -27,8 +28,9 @@ const getDCHeroes = (heroes) => {
  * ]
  */
 const convertCharactersToArray = (heroes) => {
-  return heroes;
+  return heroes.map(hero =>{ return {...hero, characters: hero.characters.split(', ')}})
 }
+
 
 /**
  * Coverts super hero data so it is grouped the publisher property
@@ -44,7 +46,18 @@ const convertCharactersToArray = (heroes) => {
  * }
  */
 const groupByPublisher = (heroes) => {
-  return heroes;
+  const groupedHero = {
+    "DC Comics": [],
+    "Marvel Comics": []
+  }
+  heroes.forEach(hero => {
+    if (hero.publisher === "DC Comics") {
+      groupedHero["DC Comics"].push(hero)
+    } else {
+      groupedHero["Marvel Comics"].push(hero)
+    }
+  })
+  return groupedHero;
 }
 
 /**
@@ -60,8 +73,15 @@ const groupByPublisher = (heroes) => {
  *  }
  * ]
  */
+
 const getDCHeroesWithMoreThanOneCharacter = (heroes) => {
-  return heroes;
+  const DCSuperhero = [];
+  convertCharactersToArray(heroes).forEach(hero => {
+    if (hero.publisher === "DC Comics" && hero.characters.length > 1) {
+      DCSuperhero.push(hero.superhero)
+    }
+  })
+  return DCSuperhero
 }
 
 module.exports = {
